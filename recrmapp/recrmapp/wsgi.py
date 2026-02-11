6dcf39f762fe1aec201eff88cc396935d22df2a3
Robert Collins Jr <robert.collinsjr@gmail.com>
2026-02-10 19:42:38 -0800

    fix django admin staticfiles #3

--- recrmapp/recrmapp/wsgi.py
+++ recrmapp/recrmapp/wsgi.py
@@ -1,12 +1,7 @@
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.middleware import WhiteNoiseMiddleware # Import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recrmapp.settings')
 
+from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
-
-# Wrap your application with WhiteNoise
-application = WhiteNoiseMiddleware(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
-
-app = application  # Important: Vercel needs this variable
